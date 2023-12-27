e11f94cd4d738a380f309689936bdc3e31cdd3d1
tomichiruss <lukadayo03@gmail.com>
2023-12-27 12:22:43 -0300

    Funciones show y hide arregladas para cualquier card - Draggable no anda

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let draggable = document.getElementById('draggable');
-let draggableheader = document.getElementById('draggableheader');
-let minimizedWelcome = document.getElementById('minimizedWelcome');
+var draggable = document.getElementById('welcomeWindow');
+var draggableheader = document.getElementById('windowDrag');
 
-let posX = 0,
+var posX = 0,
     posY = 0,
     mouseX = 0,
     mouseY = 0;
@@ -28,63 +27,17 @@ function moveElement(e) {
   draggable.style.top = mouseY + 'px';
 }
 
-function minimizarVentana(){
-  draggable.style.display = "none";
-}
-
-function showOnFooter(){
-  minimizedWelcome.style.display = "flex"
-}
-
-function showAgain(){
-  minimizedWelcome.style.display = "none";
-  draggable.style.display = "block";
+function minimizeWindow(minimizeWindowId, showMinimizedId){
+  let minimizeWindow = document.getElementById(minimizeWindowId);
+  let minimizedItem = document.getElementById(showMinimizedId);
+  minimizeWindow.style.display = "none";
+  minimizedItem.style.display = "flex";
 }
 
 
-
-
-/*//Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
-
-function dragElement(elmnt) {
-  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function show_WindowAgain(show_WindowId, hideMinimizedId){
+  let show_Window = document.getElementById(show_WindowId);
+  let hideMinimized = document.getElementById(hideMinimizedId);
+  hideMinimized.style.display = "none";
+  show_Window.style.display = "block";
 }
-*/
